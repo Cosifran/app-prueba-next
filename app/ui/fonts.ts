3f599c60cdb08db2e317ab49da26d7b9f9ac1833
Francisco Backend <90176645+Cosifran@users.noreply.github.com>
2024-02-11 21:58:29 -0500

    new project from Next

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+//Import font
+import { Inter, Lusitana } from 'next/font/google';
+//Import subconjunto de font
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] });
